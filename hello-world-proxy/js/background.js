8d1ea86b5f372f15575c96ba52f334e5f573ddd7
敖晓强 <aoxiaoqiang@163.com>
2017-07-22 21:02:09 +0800

    v1.0.3

--- hello-world-proxy/js/background.js
+++ hello-world-proxy/js/background.js
@@ -1,12 +1,19 @@
-chrome.webRequest.onAuthRequired.addListener(
-  function(details, callbackFn) {
-    console.log(details)
-    callbackFn({
-      authCredentials: {
-        username: 'jerlala@163.com',
-        password: 'Yy123457'
-      }
-    })
-  }, {
-    urls: ['<all_urls>']
-  }, ['asyncBlocking'])
+(function() {
+    chrome.webRequest.onAuthRequired.addListener(
+        function(details, callbackFn) {
+            callbackFn({
+                authCredentials: {
+                    username: 'jerlala@163.com',
+                    password: 'Yy123457'
+                }
+            })
+        }, {
+            urls: ['<all_urls>']
+        }, ['asyncBlocking']);
+
+    chrome.management.onInstalled.addListener(showConflicNumbers)
+    chrome.management.onEnabled.addListener(showConflicNumbers)
+    chrome.management.onDisabled.addListener(showConflicNumbers)
+    chrome.management.onUninstalled.addListener(showConflicNumbers)
+    showConflicNumbers();
+})()
--- hello-world-proxy/js/common.js
+++ hello-world-proxy/js/common.js
@@ -49,9 +49,8 @@ function clearStorage(key) {
  */
 function showChromeNotifications(msg, callback) {
     msg = msg || {
-        id: 'checkUserToken',
-        title: 'Notice',
-        message: 'notice info.',
+        title: 'Notice title',
+        message: 'Notice info.',
     };
     var creationCallback = callback || function() {};
     var options = {
@@ -116,6 +115,73 @@ function activeIcon() {
     });
 }
 
-document.oncontextmenu = function(){
-  return false;
+
+// 屏蔽鼠标右键
+document.oncontextmenu = function() {
+    return false;
 }
+
+// 冲突插件检测
+var Conflictor = {
+    // 获取所有冲突插件
+    getConflict: function(cb) {
+        var selfId = chrome.runtime.id;
+        var extensions = [];
+        // 获取所有浏览器安装程序
+        chrome.management.getAll(function(apps) {
+            $.each(apps, function(index, item) {
+                if (item.enabled && item.type == 'extension') {
+                    $.each(item.permissions, function(i, t) {
+                        if (t == 'proxy' && item.id != selfId) {
+                            console.log(item)
+                            extensions.push(item);
+                        }
+                    })
+                }
+            })
+            cb(extensions);
+        });
+    },
+    // 根据id禁用插件
+    disabled: function(id, name) {
+        chrome.management.setEnabled(id, false, function() {
+            showChromeNotifications({
+              title: 'Chrome插件禁用提醒',
+              message: '"' + name + '" 插件禁用成功!',
+            });
+        })
+    },
+    // 根据id卸载插件
+    uninstall: function(id, name) {
+        chrome.management.uninstall(id, function() {
+            showChromeNotifications({
+              title: 'Chrome插件卸载提醒',
+              message: '"' + name + '" 插件卸载成功!',
+            });
+        })
+    }
+}
+
+// 冲突插件个数提示
+function showConflictTips(number){
+  chrome.browserAction.setBadgeText({
+    text: number.toString()
+  })
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: [250, 46, 0, 200]
+  })
+}
+
+// 检测展示冲突数量
+function showConflicNumbers(){
+  Conflictor.getConflict(function(extensions){
+    console.log(extensions)
+    var num = extensions.length;
+    if( num === 0){
+      num = '';
+    }
+    showConflictTips(num);
+  })
+}
+
+
